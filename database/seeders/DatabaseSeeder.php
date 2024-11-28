7dbfc852a79f92c2b80da41746dc334be6ed11e3
efilonaru <naufalyasir5467@gmail.com>
2024-11-28 15:54:43 +0700

    feat: login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'username' => 'test',
+            'password' => bcrypt('password'),
         ]);
     }
 }
